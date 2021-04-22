07d118dbbeae63d72ed043778dfb6b135f15f915
Ambroise <ambroise.houndonougbo@gmail.com>
2021-04-22 16:41:07 +0200

    add

---
+++ demo.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from demopackage import Calcule
+
+n = Calcule("je suis malade")
+print(n.parler())
---
+++ demopackage/cal.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+class Calcule :
+    def __init__(self,word):
+        self.w = word
+    def parler(self):
+       return self.w
+
+
